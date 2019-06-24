0a0e17e54df49e393a65cd603589b03f7d082a2b
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-24 19:59:23 +0700

    Persist form data

--- add.php
+++ add.php
@@ -1,46 +1,44 @@
 <?php
 
-  // if (isset($_POST['submit'])) {
-  //   echo $_POST['email'];
-  //   echo $_POST['title'];
-  //   echo $_POST['ingredients'];
-  // }
+  $email = $title = $ingredients = '';
+
+  $errors = [
+    'email' => '',
+    'title' => '',
+    'ingredients' => ''
+  ];
 
   if (isset($_POST['submit'])) {
-    // echo htmlspecialchars($_POST['email']);
-    // echo htmlspecialchars($_POST['title']);
-    // echo htmlspecialchars($_POST['ingredients']);
-
     // Check email
     if (empty($_POST['email'])) {
-      echo 'Email is required! <br />';
+      $errors['email'] = 'Email is required!';
     } else {
       $email = $_POST['email'];
 
       if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo 'Email not valid! <br />';
+        $errors['email'] = 'Email not valid!';
       }
     }
 
     // Check title
     if (empty($_POST['title'])) {
-      echo 'Title is required! <br />';
+      $errors['title'] = 'Title is required!';
     } else {
       $title = $_POST['title'];
 
       if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-        echo 'Title must be letters and spaces only <br />';
+        $errors['title'] = 'Title must be letters and spaces only!';
       }
     }
 
     // Check ingredients
     if (empty($_POST['ingredients'])) {
-      echo 'At least one ingredient is required! <br />';
+      $errors['ingredients'] = 'At least one ingredient is required!';
     } else {
       $ingredients = $_POST['ingredients'];
 
       if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-        echo 'Ingredients must be a comma seperated list <br />';
+        $errors['ingredients'] = 'Ingredients must be a comma seperated list!';
       }
     }
   }
@@ -57,15 +55,18 @@
     <form action="add.php" method="POST">
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email">
+        <input type="text" class="form-control" id="email" name="email" placeholder="Enter email" value="<?php echo $email ?>">
+        <small class="form-text text-danger"><?php echo $errors['email']; ?></small>
       </div>
       <div class="form-group">
         <label for="title">Title:</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Enter pizza title">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Enter pizza title" value="<?php echo $title ?>">
+        <small class="form-text text-danger"><?php echo $errors['title']; ?></small>
       </div>
       <div class="form-group">
         <label for="ingredients">Ingredients:</label>
-        <input type="text" class="form-control" id="ingredients" name="ingredients" placeholder="Enter pizza title">
+        <input type="text" class="form-control" id="ingredients" name="ingredients" placeholder="Enter pizza title" value="<?php echo $ingredients ?>">
+        <small class="form-text text-danger"><?php echo $errors['ingredients']; ?></small>
       </div>
       <button type="submit" name="submit" class="btn btn-primary">Submit</button>
     </form>
--- templates/header.php
+++ templates/header.php
@@ -10,14 +10,12 @@
 <body>
   <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand mb-0 h1" href="#">Rahman Fadhil</a>
+      <a class="navbar-brand mb-0 h1" href="#">Ninja Pizza</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <div class="navbar-nav ml-auto">
-          <a class="nav-item nav-link active" href="#">Home</a>
-          <a class="nav-item nav-link" href="#">Pizzas</a>
           <a class="btn btn-success ml-3" href="add.php">Create a Pizza</a>
         </div>
       </div>
