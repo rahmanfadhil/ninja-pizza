dfb82a51c71001070980f5f565d0498f887aa3e5
Rahman Fadhil <rhmnfadhil@gmail.com>
2019-06-25 09:07:28 +0700

    Get data from database

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+  $conn = mysqli_connect('127.0.0.1', 'root', '12345', 'ninja_pizza');
+
+  if (!$conn) {
+    echo 'Connection Error: ' . mysqli_connect_error() ;
+  }
+
+  // Write query for all pizzas
+  $sql = 'SELECT id, title, ingredients FROM pizzas';
+
+  // Make query & get result
+  $result = mysqli_query($conn, $sql);
+
+  // Fetch the result as an array
+  $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+  print_r($pizzas);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
